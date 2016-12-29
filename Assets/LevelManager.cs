78569565c56b274bca4e9b1719b695728caa8267
Blake Fischer <bfischer@MacBook-Pro.local>
2016-12-29 12:01:56 -0800

    Completed game locally

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string levelName) {
+		Application.LoadLevel(levelName);
+	}
+
+	public void QuitRequest() {
+		Application.Quit();
+	}
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	int maxGuesses = 10;
+
+	public Text GuessText;
+
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		NextGuess();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess();
+	}
+	
+	void NextGuess () {
+		guess = Random.Range(min, max+1);
+		maxGuesses -= 1;
+
+		GuessText.text = guess.ToString();
+
+		if(maxGuesses <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+}
